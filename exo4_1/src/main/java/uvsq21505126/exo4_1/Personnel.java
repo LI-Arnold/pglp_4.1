b177bb058342fc840de180236ef7331647caf601
Utilisateur <user@debian.arcahe.ovh>
2020-03-04 16:55:42 +0100

    ajout class personnel en respectant le partern Builder

---
+++ exo4_1/src/main/java/uvsq21505126/exo4_1/Personnel.java
@@ -0,0 +1,40 @@
+package uvsq21505126.exo4_1;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Personnel {
+	private final String nom;
+	private final String prenom;
+	private final String fonction;
+	private final LocalDateTime date;
+	private final List<String> num_tel;
+	
+	public static class Builder{
+		private final String nom;
+		private final String prenom;
+		private final String fonction;
+		private final LocalDateTime date;
+		private final List<String> num_tel;
+	
+	public Builder(String nom, String prenom, String fonction, LocalDateTime date, List<String> num_tel) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.fonction = fonction;
+		this.date = date;
+		this.num_tel = num_tel;
+	}
+	
+	public Personnel build() {
+		return new Personnel(this);
+	}
+}
+	
+	private Personnel(Builder builder) {
+		this.nom = builder.nom;
+		this.prenom = builder.prenom;
+		this.fonction = builder.fonction;
+		this.date = builder.date;
+		this.num_tel = builder.num_tel;
+	}
+}
